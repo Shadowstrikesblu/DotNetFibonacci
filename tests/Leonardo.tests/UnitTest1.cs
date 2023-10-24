21bb1407f27b4c8e6f86581b235fcc2d33c4a3fa
Shadowstrikesblu <hmoulk8@gmail.com>
2023-10-24 16:03:49 +0200

    24/10 after merge

--- tests/Leonardo.tests/UnitTest1.cs
+++ tests/Leonardo.tests/UnitTest1.cs
@@ -1,10 +1,25 @@
-namespace Leonardo.tests;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leonardo.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
-        Fibonacci.RunAsync(args).Wait();
+
+        var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+        var dataBaseName = Guid.NewGuid().ToString();
+        builder.UseInMemoryDatabase(dataBaseName);
+
+        var options = builder.Options;
+        var fibonacciDataContext = new FibonacciDataContext(options);
+        await fibonacciDataContext.Database.EnsureCreatedAsync();
+        
+        var results = await new Fibonacci(fibonacciDataContext).RunAsync(new string[] { "44", "22" });
+        Assert.Equal(701408733, results[0]);
+        Assert.Equal(17711, results[1]);
+        
+        
     }
 }
